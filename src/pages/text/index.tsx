7bf25072a303aac647e7b682a21877cef7615252
henryjin3 <henry@cadenzadevelopment.com>
2021-03-02 15:10:36 -0600

    feat: add next-i18next

--- src/pages/text/index.tsx
+++ src/pages/text/index.tsx
@@ -1,3 +1,12 @@
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, [`common`])),
+  },
+});
 export default function Text() {
-  return <div>Test text</div>;
+  const { t } = useTranslation(`common`);
+  return <div>{t(`test`)}</div>;
 }
